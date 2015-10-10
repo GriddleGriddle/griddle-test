896dc073a20462f53ff77a0c597c8a2fb3ee8d4d
Ryan Lanciaux <ryan.lanciaux@gmail.com>
2015-10-10 15:57:40 -0400

    Initial

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var webpack = require('webpack');
+
+module.exports = {
+  devtool: 'source-map',
+  entry: [
+    'webpack-dev-server/client?http://localhost:3000',
+    'webpack/hot/only-dev-server',
+    './index'
+  ],
+  output: {
+    path: __dirname + '/build/',
+    filename: 'griddle.js',
+    publicPath: '/build/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      { test: /\.jsx?$/, loaders: ['react-hot', 'babel?{"plugins":["babel-plugin-object-assign"]}'], exclude: /node_modules/ }
+    ]
+  }
+};
