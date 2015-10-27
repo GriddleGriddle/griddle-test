c4f8d0379f4330ade9b3885f5a3355f163a57710
Ryan Lanciaux <ryan.lanciaux@gmail.com>
2015-10-27 00:44:59 -0400

    Working plugin registration

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 'use strict';
 
-var { Griddle } = require('griddle-render');
-var FakeData = require('./src/fake-data');
+var { Griddle, DefaultModules } = require('griddle-render');
+
+var FakeData = require('./src/fake-subgrid-data');
 var React = require('react');
 var ReactDOM = require('react-dom');
 //var RowDefinition = require('./src/row-definition');
 //var ColumnDefinition = require('./src/column-definition');
-var { GriddleRedux } = require('griddle-connector');;
-var GriddleComponent = GriddleRedux(Griddle);
+var { GriddleRedux } = require('griddle-connector');
+var SubgridPlugin = require('griddle-subgrid-plugin');
+var GriddleComponent = GriddleRedux({Griddle, Components: DefaultModules, Plugins: [SubgridPlugin] });
 
 var Test = React.createClass({
   render() {
-console.log(FakeData);
     return <div>
       <GriddleComponent data={FakeData}>
+        <DefaultModules.RowDefinition keyColumn="id">
+          <DefaultModules.ColumnDefinition id="name" displayName="Name" cssClassName="name-class" />
+          <DefaultModules.ColumnDefinition id="state" order={1} />
+        </DefaultModules.RowDefinition>
       </GriddleComponent>
     </div>
   }
