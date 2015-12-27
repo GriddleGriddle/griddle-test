5046b92b28c8d7962b381d33312d75dce82d2b84
Ryan Lanciaux <ryan.lanciaux@gmail.com>
2015-12-27 00:59:47 -0500

    Support plugins and override components

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-'use strict';
-
 var { Griddle, DefaultModules } = require('griddle-render');
 
 var React = require('react');
+var extend = require('lodash.assign');
+
 var { GriddleRedux } = require('griddle-connector');
 
 export default class extends React.Component {
   constructor(props) {
     super(props);
-    this.component = GriddleRedux({Griddle, Components: DefaultModules, Plugins: this.props.plugins });
+
+    this.component = GriddleRedux({
+      Griddle,
+      Components: extend({}, DefaultModules, this.props.components),
+      Plugins: this.props.plugins
+    });
   }
 
   render() {
